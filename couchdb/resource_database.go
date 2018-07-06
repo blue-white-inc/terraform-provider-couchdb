6d4e801617e26ef01d30f85561c6ab9e4a087947
Jonathan Dinh <jdinh0608@yahoo.com>
2018-07-06 16:01:53 -0700

    Update resource_database.go

--- couchdb/resource_database.go
+++ couchdb/resource_database.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 
 	"github.com/hashicorp/terraform/helper/schema"
-	couchdb "github.com/blue-white-inc/couchdb-go"
+	couchdb "github.com/nicolai86/couchdb-go"
 )
 
 func resourceDatabase() *schema.Resource {
